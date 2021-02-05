6314913e3dfe2d2bb9dcb59a7a95c9fdb78d8d87
santhiya <santhiya@neophrontech.com>
2021-02-05 17:06:12 +0530

    Initial commit

---
+++ app/src/main/java/com/shashank/platform/loginui/MainActivity.java
@@ -0,0 +1,26 @@
+package com.shashank.platform.loginui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView textView;
+    int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
+
+    }
+}
